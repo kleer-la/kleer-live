73800f06bfd70646649c59bb86cb64d2af21a21e
yamit <yamit.cardenas@gmail.com>
2015-12-21 16:16:29 -0500

    adjust team component size

--- public/js/custom.js
+++ public/js/custom.js
@@ -261,9 +261,9 @@ function equalHeight(container){
 
 $(document).ready(function() {
     $("#owl-team").owlCarousel({
-      items : 4, //10 items above 1000px browser width
-      itemsDesktop : [1000,3], //5 items between 1000px and 901px
-      itemsDesktopSmall : [900,2], // betweem 900px and 601px
+      items : 5, //10 items above 1000px browser width
+      itemsDesktop : [1000,4], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,3], // betweem 900px and 601px
       itemsTablet: [600,1], //2 items between 600 and 0
       itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
       stopOnHover : false,
